17a032a438228d935baccb7b4f79bcee1535eabe
andres10 <andres@piola.dev>
2020-11-13 13:43:49 -0300

    added custom function

---
+++ Assets/Scripts/ProceduralGeometry/ProceduralMeshExample.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+[ExecuteInEditMode]
+public class ProceduralMeshExample : MonoBehaviour
+{
+    private Mesh mesh;
+    public int width = 1;
+    public int height = 1;
+    public Vector3[] vertex;
+    public Color[] vertexColor;
+    
+    private void Awake()
+    {
+       
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(DrawParallel());
+    }
+
+    IEnumerator DrawParallel()
+    {
+        DrawMesh();
+        yield return new WaitForEndOfFrame();
+        StartCoroutine(DrawParallel());
+
+          
+    }
+    void GenerateMesh()
+    {
+        mesh = new Mesh();
+        GenerateTriangles();
+        DrawMesh();
+    }
+
+
+    void GenerateTriangles()
+    {
+
+   vertex = new Vector3[(width+1)*(height+1)];
+ 
+
+        for (int i=0,y = 0; y <= height; y++)
+          for (int x = 0; x <= width; x++,i++)
+            {
+                 
+                vertex[i] = new Vector3(x, y);
+                 
+          }
+
+        int[] triangles = new int[6 * width];
+        for (int ti = 0,vi=0,x=0; x < width; vi++,x++,ti+=6)
+        {
+            
+
+                triangles[ti] = vi;
+                triangles[ti+3] = triangles[ti+2]=vi+1;
+                triangles[ti + 4] = triangles[ti + 1] = vi  +width+1;
+            triangles[ti + 5] =   vi + width + 2;
+
+
+        }
+
+
+
+        mesh.vertices = vertex;
+          mesh.triangles = triangles;
+
+        vertexColor = new Color[mesh.colors.Length];
+        for (int i = 0; i < vertexColor.Length; i++)
+        {
+            mesh.colors[i] = new Color(Random.value, Random.value, Random.value);
+
+        }
+
+        //mesh.uv
+        mesh.RecalculateNormals();
+    }
+
+    void DrawMesh()
+    {
+        GetComponent<MeshFilter>().mesh = mesh;
+
+    }
+
+     
+}
---
+++ Assets/Shaders/ShaderToy/ShaderToy.hlsl
@@ -0,0 +1,18 @@
+﻿void DrawShaderToy_float (float2 screenPosition, out float3 fragColor)
+{ 
+
+   // float2 screenPosition=(2*(fragCoord.xy)-_ScreenParams.xy)/_ScreenParams.xy;
+    float x=screenPosition.x;
+    float y=screenPosition.y;
+    float timed=0.5+0.5*sin(_Time.y);
+    float a= lerp(1.0,-2.0,timed);
+    float b=lerp(-1.0,1.0,timed); 
+    float n=lerp(7.0,4.0,timed); 
+    float m= lerp(2.0,5.0,timed); 
+
+   float curves= a*sin(PI*n*x)*sin(PI*m*y)+ b*sin(PI*m*x)*sin(PI*n*y);
+    
+    float valueWithStep=step(abs(curves),0.1);
+   fragColor=float3(valueWithStep,valueWithStep,valueWithStep) ; 
+ 
+} 
