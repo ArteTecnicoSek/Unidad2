ee84d8977f01e1f9d33b9f01a0206d727cc4844a
andres10 <andres@piola.dev>
2020-11-20 14:34:30 -0300

    ramarching merge

--- Assets/Shaders/ShaderToy/ShaderToySphere.hlsl
+++ Assets/Shaders/ShaderToy/ShaderToySphere.hlsl
@@ -1,7 +1,38 @@
+
+ 
+float sdSphere( vec3 p, float s )
+{
+  return length(p)-s;
+}
+ 
+ float sdMovingSphere( vec3 p, float s )
+{
+  return length(p-vec3(2.0*cos(iTime),0,0))-s;
+}
+ 
+ 
+float opSmoothUnion( float d1, float d2, float k ) {
+    float h = clamp( 0.5 + 0.5*(d2-d1)/k, 0.0, 1.0 );
+    return mix( d2, d1, h ) - k*h*(1.0-h); }
+
+float sdTorus( vec3 p, vec2 t )
+{
+  vec2 q = vec2(length(p.xz)-t.x,p.y);
+  return length(q)-t.y;
+}
 float map(vec3 p)
 {
+   
+   vec2 torusRadius=vec2(0.5,0.2);
+
+    float d1=  sdMovingSphere(p,0.4f); 
+    float d2=sdTorus(p,torusRadius);
+
+   // float d=min(d1,d2);
+    float d=opSmoothUnion(d1,d2,0.5);
     
-    return length(p)-0.4;
+    return d;
+
     
 }
 
@@ -19,7 +50,7 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     vec2 uv = fragCoord/iResolution.xy;
 	float angle=iTime;
     vec2 screenPosition=(2.0*fragCoord.xy-iResolution.xy)/iResolution.y;
-    vec3 cameraOrigin=vec3(sin(angle),0.0,cos(angle));
+    vec3 cameraOrigin=vec3(0,1.5,2);
     vec3 cameraTarget=vec3(0.0,0.0,0.0);
     vec3 cameraForward=normalize(cameraTarget-cameraOrigin);
     vec3 cameraRight=normalize(cross(cameraForward,vec3(0.0,1.0,0.0)));
@@ -56,10 +87,10 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
     // Output to screen
     fragColor = vec4(col,1.0);
 }
-
+/*
 void DrawShaderToySphere_float (float2 screenPosition, out float3 fragColor)
 { 
 
     ///??????
  
-} 
+} */
